8a5e9b82f3f540977d3d47791f9373f8c20602e0
Quang Viet Nguyen <qv.nguyen00@gmail.com>
2022-06-14 19:42:05 +0200

    MUI theme

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../styles/globals.css'
+import React from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import theme from '../styles/theme'
 import { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material/styles'
 
 import Navbar from '../components/Navbar'
 
 function MyApp ({ Component, pageProps }: AppProps) {
+  const lightMode = useMediaQuery('(prefers-color-scheme: light)');
+
   return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
+      <ThemeProvider theme={theme(lightMode)}>
+        <Navbar />
+        <Component {...pageProps} />
+      </ThemeProvider>
   )
 }
 
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,34 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Box, Typography } from '@mui/material'
 
 export default function Home() {
   return (
-    <div className={styles.container}> 
+    <Box> 
       <Head>
         <title>Ormeli</title>
         <meta name='description' content='Ormeli App' />
         <link rel='icon' href='/carotte_assistant.png' />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <Typography variant='h1' component='div' align='center'>
           Welcome to Ormeli
-        </h1>
+        </Typography>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    </Box>
   )
 }
