f7d9e98ddced388fd3b76dbb2b96b146e5eee187
QV NGUYEN <qv.nguyen00@gmail.com>
2022-06-17 10:36:16 +0200

    change Navbar depending on user type

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { AppBar, Box, Toolbar, Typography, Button, IconButton } from '@mui/material'
+import { AppBar, Box, Toolbar, Typography, Button, IconButton, Grid } from '@mui/material'
 
 interface Props {
 
 }
 
+enum UserType {
+  patient,
+  medecin,
+  pharmacien
+}
+
 function Navbar({}: Props) {
   const user : String | null = null
   const username : String | null = null
+  const usertype : UserType | null = null
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='static'>
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between}' }}>
-          <Box sx={{ display: 'flex' }}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
             <IconButton
               size='large'
               edge='start'
@@ -22,24 +34,99 @@ function Navbar({}: Props) {
               aria-label='menu'
               sx={{ mr: 2 }}
             >
+              <Image
+              src='/favicon.png'
+              width='100%'
+              height='100%'
+              alt='Carotte Assistant'
+              />
             </IconButton>
             <Link href='/'>
               <Typography variant="h3" component="div">
                 Ormeli
               </Typography>
             </Link>
-          </Box>
-          <Box sx={{ display: 'flex', alignContent: 'center' }}>
+          </Grid>
+          {usertype == UserType.patient ?
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+          >
+            <Link href='/'>
+              <Button color='inherit'><Typography noWrap={true}>Solutions</Typography></Button>
+            </Link>
+            <Link href='/login'>
+              <Button color='inherit'><Typography noWrap={true}>Mon Espace</Typography></Button>
+            </Link>
             <Image
               src='/carotte_assistant.png'
               width='72vw'
               height='100vh'
               alt='Carotte Assistant'
             />
-            <Link href='/login'>
-              <Button color='inherit'><strong>Login</strong></Button>
+          </Grid> : 
+          usertype == UserType.medecin ?
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+          >
+            <Link href='/'>
+              <Button color='inherit'><Typography noWrap={true}>Solutions</Typography></Button>
             </Link>
-          </Box>
+            <Link href='/login'>
+              <Button color='inherit'><Typography noWrap={true}>Mon Espace</Typography></Button>
+            </Link>
+            <Image
+              src='/carotte_assistant.png'
+              width='72vw'
+              height='100vh'
+              alt='Carotte Assistant'
+            />
+          </Grid> :
+          usertype == UserType.pharmacien ?
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+          >
+            <Link href='/'>
+              <Button color='inherit'><Typography noWrap={true}>Solutions</Typography></Button>
+            </Link>
+            <Link href='/login'>
+              <Button color='inherit'><Typography noWrap={true}>Mon Espace</Typography></Button>
+            </Link>
+            <Image
+              src='/carotte_assistant.png'
+              width='72vw'
+              height='100vh'
+              alt='Carotte Assistant'
+            />
+          </Grid> :
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+          >
+            <Link href='/'>
+              <Button color='inherit'><Typography noWrap={true}>Solutions</Typography></Button>
+            </Link>
+            <Link href='/login'>
+              <Button color='inherit'><Typography noWrap={true}>Mon Espace</Typography></Button>
+            </Link>
+            <Image
+              src='/carotte_assistant.png'
+              width='72vw'
+              height='100vh'
+              alt='Carotte Assistant'
+            />
+          </Grid>
+          }
         </Toolbar>
       </AppBar>
     </Box>
