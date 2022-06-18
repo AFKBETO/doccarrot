d94f38d9431e2bcbf85c01cc99e995b7e199965b
Quang Viet Nguyen <qv.nguyen00@gmail.com>
2022-06-18 23:18:51 +0200

    patient + medecin pages

---
+++ app/pages/patient/prescriptions.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+function Prescriptions({}: Props) {
+  return (
+    <div>prescriptions</div>
+  )
+}
+
+export default Prescriptions
---
+++ app/pages/patient/suivi.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+function Suivi({}: Props) {
+  return (
+    <div>suivi</div>
+  )
+}
+
+export default Suivi
