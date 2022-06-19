53e008270adefff770a9fbfc4d5da93f54f13a79
Quang Viet Nguyen <qv.nguyen00@gmail.com>
2022-06-19 20:37:57 +0200

    user page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { AppBar, Box, Toolbar, Typography, Button, IconButton, Grid, Popper } from '@mui/material'
 
 interface Props {
-
+  posts: String
 }
 
 enum UserType {
@@ -13,26 +13,39 @@ enum UserType {
   pharmacien
 }
 
-function Navbar({}: Props) {
+function Navbar({ posts }: Props) {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const [usertype, setUserType] = React.useState<null | UserType>(null)
+  const [username, setUserName] = React.useState<null | String>(null)
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(anchorEl ? null : event.currentTarget);
   }
 
   const switchType = () => {
-    if (usertype == null) setUserType(UserType.patient)
-    else if (usertype == UserType.patient) setUserType(UserType.medecin)
-    else if (usertype == UserType.medecin) setUserType(UserType.pharmacien)
-    else if (usertype == UserType.pharmacien) setUserType(null)
+    if (usertype == null) {
+      setUserType(UserType.patient)
+      setUserName('Patient')
+    }
+    else if (usertype == UserType.patient) {
+      setUserType(UserType.medecin)
+      setUserName('Medecin')
+    }
+    else if (usertype == UserType.medecin){
+      setUserType(UserType.pharmacien)
+      setUserName('Pharmacien')
+    }
+    else if (usertype == UserType.pharmacien) {
+      setUserType(null)
+      setUserName(null)
+    }
   }
 
   const openMenuPatient = Boolean(anchorEl);
   const idMenuPatient = openMenuPatient ? 'simple-popper' : undefined;
 
   const user : String | null = null
-  const username : String | null = null
+  /*const username : String | null = null */
   /*const usertype : UserType | null = null */
 
   return (
@@ -41,9 +54,9 @@ function Navbar({}: Props) {
         <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Grid
             container
-            direction="row"
-            justifyContent="flex-start"
-            alignItems="center"
+            direction='row'
+            justifyContent='flex-start'
+            alignItems='center'
             sx={{ flexShrink: 3 }}
           >
             <IconButton
@@ -62,16 +75,16 @@ function Navbar({}: Props) {
               />
             </IconButton>
             <Link href='/'>
-              <Typography variant="h4" component="div">
+              <Typography variant='h4' component='div'>
                 Ormeli
               </Typography>
             </Link>
           </Grid>
           <Grid
             container
-            direction="row"
-            justifyContent="flex-end"
-            alignItems="center"
+            direction='row'
+            justifyContent='flex-end'
+            alignItems='center'
           >
             <Link href='/common/pourquoi'>
               <Button color='inherit'><Typography noWrap={true}>Pourquoi Ormeli ?</Typography></Button>
@@ -79,46 +92,49 @@ function Navbar({}: Props) {
             <Link href='/common/solutions'>
               <Button color='inherit'><Typography noWrap={true}>Solutions</Typography></Button>
             </Link>
-            {
-              usertype == UserType.patient ? 
-              <> 
-                <Button
-                  color='inherit'
-                  aria-describedby={idMenuPatient}
-                  type="button"
-                  onClick={handleClick}
-                > {/* navbar patient */}
-                  <Typography noWrap={true}>Mon Espace</Typography>
-                </Button>
-                <Popper id={idMenuPatient} open={openMenuPatient} anchorEl={anchorEl}>
-                  <Box sx={{ border: 1, p: 1, bgcolor: 'action.active' }}>
-                    <Link href='/patient/prescriptions'>
-                      <Button color='primary'>
-                        <Typography noWrap={true}>Prescriptions</Typography>
-                      </Button>
-                    </Link>
-                  </Box>
-                  <Box sx={{ border: 1, p: 1, bgcolor: 'action.active' }}>
-                    <Link href='/patient/suivi'>
-                      <Button color='primary'>
-                        <Typography noWrap={true}>Suivi Santé</Typography>
-                      </Button>
-                    </Link>
-                  </Box>
-                </Popper>
-              </> : 
-              usertype == UserType.medecin ?
-              <Link href='/'>
-                <Button color='inherit'> {/* navbar medecin */}
-                  <Typography noWrap={true}>Mon Espace</Typography>
-                </Button>
-              </Link> :
-              usertype == UserType.pharmacien ?
-              <Link href='/'>
-                <Button color='inherit'> {/* navbar pharmacien */}
-                  <Typography noWrap={true}>Mon Espace</Typography>
-                </Button>
-              </Link> :
+              { usertype != null ?
+                <>
+                  <Button
+                    color='inherit'
+                    aria-describedby={idMenuPatient}
+                    type='button'
+                    onClick={handleClick}
+                  >
+                    <Typography noWrap={true}>Mon Espace</Typography>
+                  </Button>
+                  <Popper id={idMenuPatient} open={openMenuPatient} anchorEl={anchorEl}>
+                    <Box sx={{ border: 1, p: 1, bgcolor: 'action.active' }}> 
+                      <Link href={`/common/${username}`}>
+                        <Button color='primary'>
+                          <Typography noWrap={true}>Mon Compte</Typography>
+                        </Button>
+                      </Link>
+                    </Box>
+                    { usertype == UserType.patient ? 
+                    <> {/* navbar patient */}
+                      <Box sx={{ border: 1, p: 1, bgcolor: 'action.active' }}> 
+                        <Link href='/patient/prescriptions'>
+                          <Button color='primary'>
+                            <Typography noWrap={true}>Prescriptions</Typography>
+                          </Button>
+                        </Link>
+                      </Box>
+                      <Box sx={{ border: 1, p: 1, bgcolor: 'action.active' }}>
+                        <Link href='/patient/suivi'>
+                          <Button color='primary'>
+                            <Typography noWrap={true}>Suivi Santé</Typography>
+                          </Button>
+                        </Link>
+                      </Box>
+                    </> : 
+                    usertype == UserType.medecin ?
+                    <> {/* navbar medecin */}
+                    </> :
+                    <> {/* navbar pharmacien */}
+                    </>}
+                  </Popper>
+                </>
+              :
               <Link href='/login'> 
                 <Button color='inherit'>{/* navbar non-connecté */}
                   <Typography noWrap={true}>Connexion</Typography>
