a4638c303748a827c00abba9881e6c0238bc9ae7
QV NGUYEN <qv.nguyen00@gmail.com>
2022-06-24 10:52:28 +0200

    add validators

--- app/pages/login/register.tsx
+++ app/pages/login/register.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import { Box, Tab, Tabs } from '@mui/material'
+import { Box, Button, FilledInput, FormControl, IconButton, InputAdornment, InputLabel, Stack, Tab, Tabs, TextField, Typography } from '@mui/material'
+import { AuthData, PatientData, MedecinData } from '../../config/types'
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
   value: number;
 }
+
 interface Props {
   
 }
 
 function TabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props
-
+  
   return (
   <Box
     role='tabpanel'
@@ -22,7 +25,12 @@ function TabPanel(props: TabPanelProps) {
     {...other}
     >
     {value === index && (
-      <Box sx={{ p: 3 }}>
+      <Box
+        component='form'
+        sx={{ p: 3 }}
+        noValidate
+        autoComplete='off'
+      >
         {children}
       </Box>
     )}
@@ -38,14 +46,63 @@ function labelProps (index: number) {
 }
 
 function Register ({}: Props) {
-  const [tabValue, setTabValue] = React.useState(0)
+  const [tabValue, setTabValue] = React.useState<number>(0)
+  const [userData, setUserData] = React.useState<AuthData>({
+    email: '',
+    password: ''
+  })
+  const [confirmationData, setConfirmationData] = React.useState<AuthData>({
+    email: '',
+    password: ''
+  })
+  const [medecinData, setMedecinData] = React.useState<MedecinData>({
+    rpps: ''
+  })
+  const [showPassword, setShowPassword] = React.useState<boolean>(false)
 
   const changeTab = (event: React.SyntheticEvent, newTabValue: number) => {
     setTabValue(newTabValue)
   }
+  const formEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserData({
+      ...userData,
+      email: event.target.value
+    })
+  }
+  const formPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserData({
+      ...userData,
+      password: event.target.value
+    })
+  }
+  const formConfirmationEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmationData({
+      ...confirmationData,
+      email: event.target.value
+    })
+  }
+  const formConfirmationPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmationData({
+      ...confirmationData,
+      password: event.target.value
+    })
+  }
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword)
+  }
+  const register = () => {
+    
+  }
 
   return (
-    <Box sx={{ border: 1, borderRadius: '20px', backgroundColor: 'primary.dark' }}>
+    <Box sx={{
+      margin: 'auto',
+      mt: 4,
+      pt: 2,
+      border: 1,
+      borderRadius: '20px',
+      backgroundColor: 'primary.dark' }}>
+      <Typography variant='h4' align='center'>Inscription</Typography>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs
           textColor='secondary'
@@ -62,13 +119,126 @@ function Register ({}: Props) {
         </Tabs>
       </Box>
       <TabPanel value={tabValue} index={0}>
-        Patient
+        <Stack
+          spacing={2}
+          justifyContent="center"
+          alignItems="center"
+          sx={{ my: 4}}
+        >
+          <TextField
+            required
+            variant='filled'
+            id='email-required'
+            label='Email'
+            type='email'
+            color='secondary'
+            sx={{
+              width: '70%',
+              color: 'text.primary'
+            }}
+            value={userData.email}
+            onInput={formEmail}
+            size='small'
+          />
+          <TextField
+            required
+            variant='filled'
+            id='email-confirm-required'
+            label='Confirmation d&apos;email'
+            type='email'
+            color='secondary'
+            sx={{
+              width: '70%',
+              color: 'text.primary'
+            }}
+            value={confirmationData.email}
+            onInput={formConfirmationEmail}
+            size='small'
+          />
+          <FormControl required sx={{ m: 1, width: '70%'}} variant="filled" size='small'>
+            <InputLabel color='secondary' htmlFor="password-required">Mot de passe</InputLabel>
+            <FilledInput
+              id='password-required'
+              color='primary'
+              type={showPassword ? 'text' : 'password'}
+              value={userData.password}
+              onInput={formPassword}
+              endAdornment={
+                <InputAdornment position='end'>
+                  <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={toggleShowPassword}
+                      onMouseDown={toggleShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <TextField
+            required
+            variant='filled'
+            id='password-confirm-required'
+            label='Confirmation de mot de passe'
+            type={showPassword ? 'text' : 'password'}
+            color='secondary'
+            sx={{
+              width: '70%',
+              color: 'text.primary'
+            }}
+            value={confirmationData.password}
+            onInput={formConfirmationPassword}
+            size='small'
+          />
+          <Button
+            variant="contained"
+            sx={{ bgcolor: 'primary.dark'}}
+            focusRipple={false}
+            onClick={register}
+          >
+            <Typography sx={{ color: 'text.primary' }}>Valider</Typography>
+          </Button>
+        </Stack>
       </TabPanel>
       <TabPanel value={tabValue} index={1}>
         Médecin - Pharmacien
       </TabPanel>
     </Box>
   )
+}
+
+function validateEmail (email: string) : boolean {
+  if (!/^[\w-.+]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) return true
+  return false
+}
+
+interface passwordError {
+  message: string
+}
+
+function validatePassword (password: string): passwordError[] {
+  const errors: passwordError[] = []
+  if (!/\d+/.test(password)) {
+    errors.push({message: 'Le mot de passe doit avoir au moins une chiffre (0-9).'})
+  }
+  if (!/[a-z]+/.test(password)) {
+    errors.push({message: 'Le mot de passe doit avoir au moins une caractère minuscule.'})
+  }
+  if (!/[A-Z]+/.test(password)) {
+    errors.push({message: 'Le mot de passe doit avoir au moins une caractère majuscule.'})
+  }
+  if (!/(\W)|(_)+/.test(password)) {
+    errors.push({message: 'Le mot de passe doit avoir au moins une caractère spéciale.'})
+  }
+  if (!/[\s\S]{8,32}/.test(password)) {
+    errors.push({message: 'Le mot de passe doit avoir 8-32 caractères.'})
+  }
+  if (!/^[^ ]+/.test(password)) {
+    errors.push({message: 'Le mot de passe ne doit pas commencer avec une espace.'})
+  }
+  return errors
 }
 
 export default Register
