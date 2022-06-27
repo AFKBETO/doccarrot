1e3780ab1e12b05f18014626d10857852525d2ce
maybee <maya.jane@orange.fr>
2022-06-27 19:19:03 +0200

    page médecin prescription V1

--- app/pages/user/[userid]/medecin/prescription.tsx
+++ app/pages/user/[userid]/medecin/prescription.tsx
@@ -1,4 +1,24 @@
 import React from 'react'
+import { styled } from '@mui/material/styles';
+import { Paper, Typography, IconButton, Grid } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
+import SendIcon from '@mui/icons-material/Send';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: 'black',
+}));
+
+const editPrescri = () => {
+
+}
+
+const sharePrescri = () => {
+
+}
 
 interface Props {
   
@@ -6,7 +26,33 @@ interface Props {
 
 function Prescription({}: Props) {
   return (
-    <div>prescriptions</div>
+    <Grid container spacing={2} sx={{paddingLeft: 5, paddingRight:5, paddingBottom: 10}}>
+      <Grid item xs={10}>
+        <Typography><h1>Création d'une prescription</h1></Typography>
+      </Grid>
+      <Grid item xs={11}>
+        <Item sx={{background: '#ABBD98', borderRadius: 5}}>
+          <Typography><h1>Espace édition</h1></Typography>
+        </Item>
+      </Grid>
+      <Grid item xs={1}>
+        <Item sx={{background: '#ABBD98', borderRadius: 5}}>
+          <Grid container spacing={2} direction='column'>
+            <Grid item xs={1}>
+              <IconButton variant="contained" component="span" onClick={editPrescri}>
+                <EditIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs={1}>
+              <IconButton variant="contained" component="span" onClick={sharePrescri}>
+                <SendIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Item>
+      </Grid>
+      
+    </Grid>
   )
 }
 
