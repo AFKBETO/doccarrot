aec210ef12dd58cc9b2efa5d59dd4884f15c3d2e
GuillaumeVDN <vandenneucker.guillaume@gmail.com>
2022-06-29 00:23:05 +0200

    Some filler text

--- app/pages/common/solutions.tsx
+++ app/pages/common/solutions.tsx
@@ -1,10 +1,33 @@
+import {Grid, IconButton, Typography} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import SendIcon from "@mui/icons-material/Send";
+import React from "react";
+import {textAlign} from "@mui/system";
+
 interface Props {
   
 }
 
 function Solutions({}: Props) {
   return (
-    <div>solutions</div>
+      <Grid container sx={{ paddingLeft: 5, paddingRight:5, paddingBottom: 10 }}>
+          <Typography><h1>Nos solutions</h1></Typography>
+          <p style={{ textAlign: 'justify' }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero orci, viverra nec tortor nec, accumsan posuere risus. Aenean mollis justo at tempor sodales. Etiam ullamcorper urna vel ante auctor, id dictum ex vehicula. Morbi in finibus dolor, id sagittis enim. Praesent a pellentesque sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam pharetra est nec mauris posuere fringilla. Integer vitae malesuada mauris, sit amet congue libero. Sed justo leo, consequat eget mollis sed, lobortis nec tellus.
+          </p>
+          <p style={{ textAlign: 'justify' }}>
+              Sed at tincidunt risus. Integer suscipit viverra suscipit. Etiam pellentesque sagittis ipsum nec rutrum. Sed dapibus dignissim quam. Nunc nec porta purus, vitae faucibus tortor. Curabitur a congue orci, nec aliquam risus. Donec cursus pulvinar justo. Phasellus id massa et massa sodales rhoncus.
+          </p>
+          <p style={{ textAlign: 'justify' }}>
+              Suspendisse ultrices nisi nec nisl ullamcorper, ac molestie tellus facilisis. Curabitur non sem ultricies, tempus risus at, euismod quam. Morbi ac consequat eros. Mauris ac condimentum nisi. Duis lobortis enim nisl, ut pellentesque odio elementum id. Vestibulum ligula nunc, gravida id magna in, rutrum tincidunt sapien. Pellentesque lobortis dictum laoreet.
+          </p>
+          <p style={{ textAlign: 'justify' }}>
+              Mauris eget placerat augue, ultricies sollicitudin arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris velit risus, sagittis eu bibendum et, varius id nibh. Donec vehicula vulputate tincidunt. Suspendisse et neque dui. Sed pharetra lobortis ex, sed ornare nisi luctus sit amet. Maecenas at mattis dolor. Mauris vitae tincidunt odio. Suspendisse at imperdiet mauris. Proin a metus et orci auctor egestas sit amet vitae leo.
+          </p>
+          <p style={{ textAlign: 'justify' }}>
+              Nulla eget arcu ante. Phasellus nec mollis odio, id maximus urna. Nullam venenatis suscipit mi quis ultricies. Proin sit amet neque aliquam felis facilisis hendrerit sed in elit. Vivamus urna erat, facilisis vitae elit eget, vestibulum fermentum est. Duis rutrum vitae quam ac fringilla. Nunc hendrerit, mi et porttitor ornare, tortor ipsum luctus sapien, id elementum odio turpis quis odio. Etiam sapien ex, tincidunt ut diam quis, tristique laoreet purus. Aliquam sit amet lorem quis dolor hendrerit elementum eu sed arcu. Vivamus commodo tristique tellus ut lobortis. Phasellus tempor hendrerit est molestie cursus. Nunc fermentum lectus sapien, non euismod leo iaculis ultrices. Duis sed felis quis nisl aliquam porta a sit amet eros. Donec id risus luctus, viverra sem eu, accumsan mauris. Mauris semper urna massa, id iaculis arcu euismod sed.
+          </p>
+      </Grid>
   )
 }
 
