a91f7fc42ea2761f77f08cae799be3823e549900
QV NGUYEN <qv.nguyen00@gmail.com>
2022-06-29 16:51:37 +0200

    footer tweak

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -13,42 +13,41 @@ import { Toaster } from 'react-hot-toast'
 import { Box } from "@mui/material"
 
 const bodyStyle = {
-    'height': '90vh',
-    'margin': 0,
-    'display': 'flex',
-    'flex-direction': 'column'
-};
-const footerStyle = {
-    'height': '10vh',
-    'margin-top': 'auto'
-};
+  'maxHeight': '90vh',
+  'margin': 0,
+  'display': 'flex',
+  'flex-direction': 'column'
+}
 
 function MyApp ({ Component, pageProps }: AppProps) {
-    //const lightMode = useMediaQuery('(prefers-color-scheme: light)');
+  //const lightMode = useMediaQuery('(prefers-color-scheme: light)');
 
-    // récupération des données actuellement en cache depuis Firebase
-    const { userId, userName, userType, firebaseUser } = useUserData()
-    const userContext = React.useContext(USER_CONTEXT)
-    
-    userContext.updateUserId(userId)
-    userContext.updateUserName(userName)
-    userContext.updateFirebaseUser(firebaseUser)
-    userContext.updateUserType(userType)
+  // récupération des données actuellement en cache depuis Firebase
+  const { userId, userName, userType, firebaseUser } = useUserData()
+  const userContext = React.useContext(USER_CONTEXT)
+  
+  userContext.updateUserId(userId)
+  userContext.updateUserName(userName)
+  userContext.updateFirebaseUser(firebaseUser)
+  userContext.updateUserType(userType)
 
-    return (
-        <ThemeProvider theme={theme()}>
-            <Box sx={bodyStyle}>
-                <USER_CONTEXT.Provider value={userContext}>
-                    <Navbar />
-                    <Component {...pageProps} />
-                </USER_CONTEXT.Provider>
-                <Toaster />
-            </Box>
-            <Box sx={footerStyle}>
-                <Footer />
-            </Box>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider theme={theme()}>
+      <Box sx={{
+        position:'relative',
+        maxHeight: '70vh'
+      }}>
+        <Box sx={bodyStyle}>
+          <USER_CONTEXT.Provider value={userContext}>
+            <Navbar />
+            <Component {...pageProps} />
+          </USER_CONTEXT.Provider>
+          <Toaster />
+        </Box>
+        <Footer />
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
